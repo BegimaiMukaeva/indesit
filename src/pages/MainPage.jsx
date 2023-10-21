969bddcd4399410b71fab20121c5c8a31f67fabe
Begimai Mukaeva <begimaimukaeva2001@gmail.com>
2023-10-22 00:19:57 +0600

    after sercice

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -4,12 +4,14 @@ import MainPageBanner from '../ui/mainPageBanner/MainPageBanner';
 import CategoriesBlock from "../components/CategoriesBlock/CategoriesBlock";
 import CategoryText from '../ui/categoryText/CategoryText';
 import FooterBlock from '../components/FooterBlock/FooterBlock'
+import ServiceBlock from '../components/ServiceBlock/ServiceBlock'
 
 const MainPage = () => {
     return (
         <div>
             <HeaderBlock />
             <MainPageBanner />
+            <ServiceBlock />
             <CategoryText />
             <CategoriesBlock />
             <FooterBlock />
